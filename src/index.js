c9cb583f71cc77251ce5d69b05b4cc35aab103a0
Ekaterina <Kata_Yurevna@mail.ru>
2021-09-08 18:42:52 +0700

    JSX-синтаксис

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const AppList = () => {
+  const items = ['Items 1', 'Items 2', 'Items 3', 'Items 4'];
+  const firstItems = <li>Items 0</li>;
+  const isAuth = false;
   return (
     <ul>
-      <li>My First Element</li>
-      <li>My Second Element</li>
+      {isAuth ? firstItems : null}
+      {items}
+      {items.map((item) => (
+        <li>{item}</li>
+      ))}
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
     </ul>
   );
 };
@@ -14,12 +22,22 @@ const AppList = () => {
 //   return <h1>This is my Header!</h1>;
 // };
 
-const AppHeader = () => <h1>This is my Header!</h1>;
+const AppHeader = () => <h1 className="App-header">This is my Header!</h1>;
+
+const AppInput = () => {
+  const placeholder = 'Type text...';
+  return (
+    <label htmlFor="search">
+      <input id="search" placeholder={placeholder} />
+    </label>
+  );
+};
 
 const App = () => {
   return (
     <>
       <AppHeader />
+      <AppInput />
       <AppList />
       <AppHeader />
       <AppList />
