e8670ca390c6b633d61a86a23243010e70a2ea38
Ekaterina <Kata_Yurevna@mail.ru>
2021-09-16 23:41:47 +0700

    task3

--- src/routes/Game/index.js
+++ src/routes/Game/index.js
@@ -1,5 +1,48 @@
+import { useState } from 'react';
+import s from './style.module.css';
+import POKEMONS from '../../pokemons.js';
+import Layout from '../../components/Layout';
+import PokemonCard from '../../components/PokemonCard';
+
+const POKEMONS_LIST = POKEMONS.map((pokemon) => {
+  Object.assign(pokemon, { active: false });
+  return pokemon;
+});
+
 const GamePage = () => {
-  return <div>This is Game Page!!!</div>;
+  const [pokemons, setPokemons] = useState(POKEMONS_LIST);
+
+  const handleClickPokemon = (id) => {
+    setPokemons(
+      POKEMONS_LIST.map((item) => {
+        if (item.id === id) {
+          item.active = !item.active;
+        }
+        return item;
+      })
+    );
+  };
+
+  return (
+    <>
+      <Layout title="GAME" desc="Описание" colorBg="red">
+        <div className={s.flex}>
+          {pokemons.map((item) => (
+            <PokemonCard
+              key={item.id}
+              name={item.name}
+              img={item.img}
+              id={item.id}
+              type={item.type}
+              values={item.values}
+              isActive={item.active}
+              onClickPokemon={handleClickPokemon}
+            />
+          ))}
+        </div>
+      </Layout>
+    </>
+  );
 };
 
 export default GamePage;
