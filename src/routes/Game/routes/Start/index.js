be9c83073acbe2537c52d32684be98d546278869
Ekaterina <Kata_Yurevna@mail.ru>
2021-09-22 00:43:22 +0700

    работа с контекстом

---
+++ src/routes/Game/routes/Start/index.js
@@ -0,0 +1,81 @@
+import { useState, useEffect, useContext } from 'react';
+import { FireBaseContext } from '../../../../context/firebaseContext';
+import { PokemonContext } from '../../../../context/pokemonContext';
+import s from './style.module.css';
+import Layout from '../../../../components/Layout';
+import PokemonCard from '../../../../components/PokemonCard';
+import { useHistory } from 'react-router';
+
+const StartPage = () => {
+  const firebase = useContext(FireBaseContext);
+  const pokemonsContext = useContext(PokemonContext);
+  const history = useHistory();
+  console.log('pokemonsContext: ', pokemonsContext);
+  const [pokemons, setPokemons] = useState({});
+
+  useEffect(() => {
+    firebase.getPokemonSoket((pokemons) => {
+      setPokemons(pokemons);
+    });
+
+    return () => firebase.offPokemonSoket();
+  }); //React Hook useEffect has a missing dependency: 'firebase'. Either include it or remove the dependency array
+
+  const handleChangeSelected = (key) => {
+    const pokemon = { ...pokemons[key] };
+    pokemonsContext.onSelectedPokemons(key, pokemon);
+
+    setPokemons((prevState) => ({
+      ...prevState,
+      [key]: {
+        ...prevState[key],
+        selected: !prevState[key].selected,
+      },
+    }));
+  };
+
+  const handleStartGameClick = () => {
+    history.push('/game/board');
+  };
+
+  return (
+    <>
+      <Layout title="GAME" desc="Описание" colorBg="red">
+        <button
+          onClick={handleStartGameClick}
+          disabled={Object.keys(pokemonsContext.pokemons).length < 5}
+          className={s.button}
+        >
+          Start Game
+        </button>
+        <div className={s.flex}>
+          {Object.entries(pokemons).map(
+            ([key, { name, img, id, type, values, selected }]) => (
+              <PokemonCard
+                className={s.card}
+                key={key}
+                name={name}
+                img={img}
+                id={id}
+                type={type}
+                values={values}
+                isActive={true}
+                isSelected={selected}
+                onClickPokemon={() => {
+                  if (
+                    Object.keys(pokemonsContext.pokemons).length < 5 ||
+                    selected
+                  ) {
+                    handleChangeSelected(key);
+                  }
+                }}
+              />
+            )
+          )}
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default StartPage;
